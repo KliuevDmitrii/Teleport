4943e0ad8f6d5420114959ff45cc3ff52e36531b
Kliuev Dmitrii <dimik1986@gmail.com>
2025-02-21 16:04:45 +0400

    add test auth admin

--- Test/test.py
+++ Test/test.py
@@ -1,6 +1,13 @@
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+
 from pages.AuthPage import AuthPage
 
-def first_test(browser):
-
+def auth_test(browser):
     auth_page = AuthPage(browser)
     auth_page.go()
+    auth_page.login_as("123456", "123456")
+    WebDriverWait(browser, 10).until(EC.url_to_be("https://ratzzzsmsbot.ru/admin/#/transfers"))
+
+    assert auth_page.get_current_url().endswith("admin/#/transfers")
--- pages/AuthPage.py
+++ pages/AuthPage.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver 
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
 
 class AuthPage:
 
@@ -9,3 +11,11 @@ class AuthPage:
 
     def go(self):
         self.__driver.get(self.__url)
+
+    def login_as(self, email: str, password: str):
+        self.__driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Логин или эл. почта"]').send_keys(email)
+        self.__driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Пароль"]').send_keys(password)
+        self.__driver.find_element(By.CSS_SELECTOR, 'span.p-button-label[data-pc-section="label"]').click()
+
+    def get_current_url(self):
+        return self.__driver.current_url
