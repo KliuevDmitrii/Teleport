05408220d899819cd0f2dba05a6ed3fa83cfddeb
Kliuev Dmitrii <dimik1986@gmail.com>
2025-02-23 12:32:29 +0400

    reserch test

--- Test/test.py
+++ Test/test.py
@@ -1,13 +1,25 @@
+import pytest
+import allure
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 
 from pages.AuthPage import AuthPage
 
-def auth_test(browser):
+# Проверка авторизация зарегестрированного пользователя
+@pytest.mark.parametrize("email, password", [
+    ("123456", "123456")
+])
+def auth_test(browser, email, password):
     auth_page = AuthPage(browser)
     auth_page.go()
-    auth_page.login_as("123456", "123456")
+    auth_page.enter_email(email)
+    auth_page.enter_password(password)
+    auth_page.click_button()
     WebDriverWait(browser, 10).until(EC.url_to_be("https://ratzzzsmsbot.ru/admin/#/transfers"))
+    
+    with allure.step(f"Текущий URL: {auth_page.get_current_url()}"):
+       pass  # Логируем URL в отчете Allure
 
-    assert auth_page.get_current_url().endswith("admin/#/transfers")
+    with allure.step("Проверка редиректа после авторизации"):
+        assert auth_page.get_current_url().endswith("admin/#/transfers"), "Редирект не произошел или URL некорректный"
--- pages/AuthPage.py
+++ pages/AuthPage.py
@@ -1,20 +1,37 @@
+import allure
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 
 class AuthPage:
+    """
+    Этот класс предоставляет методы для выполнения действий на странице авторизации пользователя
+    """
 
     def __init__(self, driver: WebDriver) -> None:
         self.__url = "https://ratzzzsmsbot.ru/admin/#/login?returnTo=/profile"
         self.__driver = driver
 
+    @allure.step("Открыть страницу авторизации")
     def go(self):
         self.__driver.get(self.__url)
 
-    def login_as(self, email: str, password: str):
-        self.__driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Логин или эл. почта"]').send_keys(email)
-        self.__driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Пароль"]').send_keys(password)
+    @allure.step("Ввести email")
+    def enter_email(self, email: str):
+        element = self.__driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Логин или эл. почта"]')
+        element.clear()
+        element.send_keys(email)
+
+    @allure.step("Ввести пароль")
+    def enter_password(self, password:str):
+        element = self.__driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Пароль"]')
+        element.clear()
+        element.send_keys(password)
+
+    @allure.step("Нажать кнопку 'Войти'")
+    def click_button(self):
         self.__driver.find_element(By.CSS_SELECTOR, 'span.p-button-label[data-pc-section="label"]').click()
 
     def get_current_url(self):
